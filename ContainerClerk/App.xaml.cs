7b67a4265f42dc07b81ec1d42b3841b1d4bcae56
Patrik Kun <kunpatrik00@gmail.com>
2025-07-01 17:01:32 +0200

    eol check

--- ContainerClerk/App.xaml.cs
+++ ContainerClerk/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using NLog;
+﻿using System.IO;
+using NLog;
 
 namespace ContainerClerk;
 
@@ -9,6 +10,13 @@ public partial class App
     private App()
     {
         Logger.Info("Application started.");
+        
+        Logger.Info("Checking scripts for EOL.");
+        foreach (var file in Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Scripts")))
+        {
+            File.WriteAllText(file,File.ReadAllText(file).Replace("\r\n", "\n"));
+        }
+        Logger.Info("Scripts have been saved.");
     }
 
     ~App()
